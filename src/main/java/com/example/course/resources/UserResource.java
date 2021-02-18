47cdf5933b75ed31616ac8630b23ba7d70d7f10b
Tiago Maia PC <tiagoferreira209@gmail.com>
2021-02-18 16:27:15 -0300

    User entity and resorce

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Tiago Maia", "tiagoferreira209gmail.com", "85 997515694", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
